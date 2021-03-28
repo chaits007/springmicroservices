dba7b87e4d9b675dbfe3d955a5debc566523b935
chaits007 <vuppuluri@gmail.com>
2021-03-28 22:51:06 +0530

    DB

--- currency-exchange-service/src/main/java/com/chaits/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/chaits/microservices/controller/CurrencyExchangeController.java
@@ -3,6 +3,7 @@ package com.chaits.microservices.controller;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,12 @@ import com.chaits.microservices.model.ExchangeValue;
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue getExchangeRate(@PathVariable String from, @PathVariable String to) {
-		return new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		return new  ExchangeValue(1000L, from, to, BigDecimal.valueOf(65), Integer.parseInt(environment.getProperty("local.server.port")));
 	}
 
 }
--- currency-exchange-service/src/main/java/com/chaits/microservices/model/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/chaits/microservices/model/ExchangeValue.java
@@ -2,14 +2,34 @@ package com.chaits.microservices.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
+@Entity
+@Table(name="Exchange_Value")
 @Data
 @AllArgsConstructor
 public class ExchangeValue {
+	
+	@Id
+	@Column(name="id")
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
+	
+	@Column(name="conversion_multiple")
 	private BigDecimal conversionMultiple;
+	
+	@Column(name="port")  
+	private int port;  
 }
